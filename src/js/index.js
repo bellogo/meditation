9bfefb6b07fcee49d6b2e6be1856d8f8fa530fbc
Ufuoma Ogodo <ufuomaogodo@gmail.com>
2020-06-03 17:00:21 +0100

    wrote js and media query styling for third section and edited some general styling

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 const html = document.querySelector('html');
 const waves = document.querySelector('#waves');
-const ulContainer = document.querySelector('.ulContainer')
+const ulContainer = document.querySelector('.ulContainer');
+const thirdSection = document.querySelector('.thirdSection');
 
 const resizePage = () => {
     if (window.innerWidth > 768) {
@@ -14,6 +15,13 @@ const resizePage = () => {
     }
     if (window.innerWidth < 768) {
         html.style.fontSize = "15px";
+        let defaultFontSize = 15;
+        let defaultScreenSize = 768;
+        let currentScreenSize = window.innerWidth;
+        let fontdecreaseRate = defaultFontSize / defaultScreenSize;
+        let newFontSize = defaultFontSize - ((defaultScreenSize - currentScreenSize) * fontdecreaseRate);
+        newFontSize += 'px'
+        thirdSection.style.fontSize = newFontSize;
     }
 }
 window.addEventListener('resize', () => {
